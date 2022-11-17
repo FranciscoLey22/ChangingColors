6fe685f42478f027a574507f2ac7b8d1d9eeed11
Francisco Ley <francisco.ley@gmail.com>
2022-11-17 09:36:19 -0500

    changed color from red to green in top right mouse position

--- changingcolors.js
+++ changingcolors.js
@@ -3,7 +3,7 @@ body.addEventListener('pointermove', function(event){
     let width = (event.pageX / window.innerWidth);
     let height = (event.pageY / window.innerHeight); 
 
-    console.log(`rgb(${width*255}, 0, ${height*255})`);
-    body.style.backgroundColor = `rgb(${width*255}, 0, ${height*255})`;
+    console.log(`(0, rgb(${width*255}, ${height*255})`);
+    body.style.backgroundColor = `rgb(0,${width*255}, ${height*255})`;
 
 });
